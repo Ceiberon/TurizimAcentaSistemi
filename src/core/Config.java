3e5e3aeec02ab4f7891377830789d9d6e815192f
Ceiberon <ethem_641@hotmail.com>
2024-04-24 03:43:42 +0300

    Bugfix

---
+++ src/core/Config.java
@@ -0,0 +1,7 @@
+package core;
+public class Config {
+    public final static String PROJECT_TITLE = "Patika Tourism Agency";
+    public final static String DB_URL = "jdbc:mysql://localhost/trzm";
+    public final static String DB_USERNAME = "root";
+    public final static String DB_PASSWORD = "mysql";
+}
